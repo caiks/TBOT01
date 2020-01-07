87db6d9a325682bfc7e014fd7a7eb34554523596
Cliff McCartney <caiks@greenlake.co.uk>
2020-01-07 11:57:02 +0000

    Add files via upload

---
+++ TBOT01Dev.h
@@ -0,0 +1,69 @@
+﻿#ifndef TBOT01DEV_H
+#define TBOT01DEV_H
+
+#include "AlignmentUtil.h"
+#include "Alignment.h"
+#include "AlignmentApprox.h"
+#include "AlignmentAeson.h"
+#include "AlignmentRepa.h"
+#include "AlignmentAesonRepa.h"
+#include "AlignmentRandomRepa.h"
+#include "AlignmentPracticableRepa.h"
+#include "AlignmentPracticableIORepa.h"
+
+#include <iomanip>
+#include <set>
+#include <unordered_set>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include <string>
+
+
+namespace TBOT01
+{
+    struct Bitmap
+    {
+	Bitmap(int h = 1, int w = 1, unsigned char x = 0) {
+	    height = h;
+	    width = w;
+	    image.resize(h*w*3,x);
+	}
+	int height;
+	int width;
+	std::vector<unsigned char> image;
+    };
+
+    Bitmap bminsert(const Bitmap&, int, int, const Bitmap&);
+    Bitmap bmborder(int, const Bitmap&);
+    Bitmap bmhstack(const std::vector<Bitmap>&);
+    Bitmap bmvstack(const std::vector<Bitmap>&);
+
+    void bmwrite(std::string, const Bitmap&);
+
+    Bitmap hrbm(int,int,int,const Alignment::HistoryRepa&);
+
+
+    typedef std::tuple<std::unique_ptr<Alignment::System>, std::unique_ptr<Alignment::SystemRepa>, std::unique_ptr<Alignment::HistoryRepa>> SystemHistoryRepaTuple;
+
+    // trainBucketedIO :: Int -> IO (System, HistoryRepa)
+    SystemHistoryRepaTuple trainBucketedIO(int);
+
+    // trainBucketedAffineIO :: Int -> Int -> Double -> Int -> IO (System, HistoryRepa)
+    SystemHistoryRepaTuple trainBucketedAffineIO(int, int, double, int);
+
+    // trainBucketedIO :: Int -> IO (System, HistoryRepa)
+    SystemHistoryRepaTuple trainBucketedRegionRandomIO(int,int,int);
+
+    // testBucketedIO :: Int -> IO (System, HistoryRepa)
+    SystemHistoryRepaTuple testBucketedIO(int);
+
+}
+
+
+#endif
---
+++ controller.h
@@ -0,0 +1,60 @@
+﻿// https://github.com/ROBOTIS-GIT/turtlebot3_simulations/blob/ros2-devel/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_drive.hpp
+
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#define DEG2RAD (M_PI / 180.0)
+#define RAD2DEG (180.0 / M_PI)
+
+#define CENTER 0
+#define LEFT   1
+#define RIGHT  2
+
+#define LINEAR_VELOCITY  0.3
+#define ANGULAR_VELOCITY 1.5
+
+#define GET_TB3_DIRECTION 0
+#define TB3_DRIVE_FORWARD 1
+#define TB3_RIGHT_TURN    2
+#define TB3_LEFT_TURN     3
+
+class Controller : public rclcpp::Node
+{
+public:
+    Controller();
+    ~Controller();
+
+private:
+    // ROS topic publishers
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+
+    // ROS topic subscribers
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+
+    // Variables
+    double robot_pose_;
+    double prev_robot_pose_;
+    double scan_data_[3];
+
+    // ROS timer
+    rclcpp::TimerBase::SharedPtr update_timer_;
+
+    // Function prototypes
+    void update_callback();
+    void update_cmd_vel(double linear, double angular);
+    void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+};
+#endif // CONTROLLER_H
