e53d1341b7b12686af439017ff11d47f7f99d340
Cliff McCartney <caiks@greenlake.co.uk>
2020-01-23 14:39:15 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ using namespace std;
 typedef std::chrono::duration<double> sec; 
 typedef std::chrono::high_resolution_clock clk;
 
+#define ECHO(x) cout << #x << endl; x
+#define EVAL(x) cout << #x << " = " << (x) << endl
+#define TRUTH(x) cout << #x << " = " << ((x) ? "true" : "false") << endl
+
 int main(int argc, char **argv)
 {
     if (false)
@@ -38,7 +42,7 @@ int main(int argc, char **argv)
 	in2.close();
     }
 
-    if (true)
+    if (false)
     {
 	std::ifstream in("202001222010_2.TBOT01.bin", std::ios::binary);
 	auto rr = persistentsRecordList(in);
@@ -53,6 +57,145 @@ int main(int argc, char **argv)
 	in.close();
     }
 
+    if (true)
+    {
+	std::ifstream in("202001222010_2.TBOT01.bin", std::ios::binary);
+	auto rr = persistentsRecordList(in);
+	in.close();
+
+	EVAL(rr->size());
+	EVAL(rr->front().id);
+	EVAL(rr->front().ts);
+	EVAL(rr->back().id);
+	EVAL(rr->back().ts);
+
+	/*
+	rr->size() = 279
+	rr->front().id = 0
+	rr->front().ts = 0.252699
+	rr->back().id = 278
+	rr->back().ts = 69.7564
+	*/
+
+	std::vector<double> sensor_pose[7];
+	std::vector<double> sensor_scan;
+	std::vector<double> action_linear;
+	std::vector<double> action_angular;
+	for (auto& r : *rr)
+	{
+	    for (std::size_t i = 0; i < 7; i++)
+		sensor_pose[i].push_back(r.sensor_pose[i]);
+	    for (std::size_t i = 0; i < 360; i++)
+		sensor_scan.push_back(r.sensor_scan[i]);
+	    action_linear.push_back(r.action_linear);
+	    action_angular.push_back(r.action_angular);
+	}
+	for (std::size_t i = 0; i < 7; i++)
+	    std::sort(sensor_pose[i].begin(), sensor_pose[i].end());
+	std::sort(sensor_scan.begin(), sensor_scan.end());
+	std::sort(action_linear.begin(), action_linear.end());
+	std::sort(action_angular.begin(), action_angular.end());
+	for (std::size_t i = 0; i < 7; i++)
+	{
+	    EVAL(i);
+	    EVAL(sensor_pose[i].front());
+	    EVAL(sensor_pose[i].back());
+	}
+	EVAL(sensor_scan.front());
+	EVAL(sensor_scan.back());
+	EVAL(action_linear.front());
+	EVAL(action_linear.back());
+	EVAL(action_angular.front());
+	EVAL(action_angular.back());
+
+	/*
+	i = 0
+	sensor_pose[i].front() = -1.97511
+	sensor_pose[i].back() = 4.35613
+	i = 1
+	sensor_pose[i].front() = -11.2565
+	sensor_pose[i].back() = 1.50003
+	i = 2
+	sensor_pose[i].front() = 0.0083258
+	sensor_pose[i].back() = 0.0107955
+	i = 3
+	sensor_pose[i].front() = -0.0210602
+	sensor_pose[i].back() = 0.00900391
+	i = 4
+	sensor_pose[i].front() = -0.0261257
+	sensor_pose[i].back() = 0.0302976
+	i = 5
+	sensor_pose[i].front() = -0.972794
+	sensor_pose[i].back() = 0.150416
+	i = 6
+	sensor_pose[i].front() = 0.231651
+	sensor_pose[i].back() = 0.999958
+	sensor_scan.front() = 0.12
+	sensor_scan.back() = 3.5
+	action_linear.front() = 0
+	action_linear.back() = 0.3
+	action_angular.front() = -1.5
+	action_angular.back() = 1.5
+	*/
+
+	std::vector<double> sensor_scan_range{ 0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0 };
+	std::map<double, std::size_t> sensor_scan_dist;
+	for (auto x : sensor_scan_range)
+	    sensor_scan_dist[x] = 0;
+	for (auto y : sensor_scan)
+	    for (auto x : sensor_scan_range)
+		if (y <= x)
+		{
+		    sensor_scan_dist[x]++;
+		    break;
+		}
+	EVAL(sensor_scan_dist);
+	/*
+	sensor_scan_dist = {(0.5,4391),(1,17001),(1.5,8267),(2,5194),(2.5,2919),(3,2160),(3.5,60508),(4,0)}
+	*/
+
+	for (std::size_t i = 0; i < sensor_scan.size() - 60508; i += (sensor_scan.size() - 60508) / 7)
+	{
+	    EVAL(i);
+	    EVAL(sensor_scan[i]);
+	}
+	/*
+	i = 0
+	sensor_scan[i] = 0.12
+	i = 5704
+	sensor_scan[i] = 0.519557
+	i = 11408
+	sensor_scan[i] = 0.629643
+	i = 17112
+	sensor_scan[i] = 0.811073
+	i = 22816
+	sensor_scan[i] = 1.07406
+	i = 28520
+	sensor_scan[i] = 1.42758
+	i = 34224
+	sensor_scan[i] = 1.91638
+	i = 39928
+	sensor_scan[i] = 2.99914
+	*/
+
+	std::set<double> action_linear_dist;
+	for (auto y : action_linear)
+	    action_linear_dist.insert(y);
+	EVAL(action_linear_dist);
+
+	/*
+	action_linear_dist = {0,0.3}
+	*/
+
+	std::set<double> action_angular_dist;
+	for (auto y : action_angular)
+	    action_angular_dist.insert(y);
+	EVAL(action_angular_dist);
+	/*
+	action_angular_dist = {-1.5,0,1.5}
+	*/
+
+    }
 
     return 0;
 }
