a50d87f0aa9c628717ddd3081e5dc56033c866a6
Cliff McCartney <caiks@greenlake.co.uk>
2020-07-24 16:07:42 +0100

    Actor

---
+++ actor.cpp
@@ -0,0 +1,366 @@
+#include "observer.h"
+
+using namespace Alignment;
+using namespace TBOT01;
+using namespace std;
+using namespace std::chrono_literals;
+
+typedef std::chrono::duration<double> sec;
+typedef std::chrono::high_resolution_clock clk;
+
+#define ECHO(x) cout << #x << endl; x
+#define EVAL(x) cout << #x << ": " << (x) << endl
+#define EVALL(x) cout << #x << ": " << endl << (x) << endl
+#define TRUTH(x) cout << #x << ": " << ((x) ? "true" : "false") << endl
+
+Actor::Actor(const std::string& model, const std::string& room_initial, std::chrono::milliseconds observe_interval, const std::string& dataset)
+: Node("TBOT01_actor_node")
+{
+	auto hrsel = eventsHistoryRepasHistoryRepaSelection_u;
+	auto hrred = setVarsHistoryRepasReduce_u;
+		
+	_pose_updated = false;
+	_scan_updated = false;
+	
+	_room = room_initial;
+	
+	std::unique_ptr<Alignment::HistoryRepa> hr;
+	{
+		std::vector<std::string> files{
+			"data002_room1.bin",
+			"data002_room2.bin",
+			"data002_room2_2.bin",
+			"data002_room3.bin",
+			"data002_room4.bin",
+			"data002_room5.bin",
+			"data002_room5_2.bin"
+		};
+		if (dataset == "data003")
+		{
+			files.clear();
+			files.push_back("data003.bin");
+		}
+		else if (dataset == "data004")
+		{
+			files.clear();
+			files.push_back("data003.bin");
+			files.push_back("data004_01.bin");
+			files.push_back("data004_02.bin");
+			files.push_back("data004_03.bin");
+			files.push_back("data004_04.bin");
+			files.push_back("data004_05.bin");
+		}
+		else if (dataset != "data002")
+		{
+			files.clear();
+			files.push_back(dataset+".bin");
+		}			
+		HistoryRepaPtrList ll;
+		for (auto& f : files)
+		{
+			std::ifstream in(f, std::ios::binary);
+			auto qq = persistentsRecordList(in);
+			in.close();
+			SystemHistoryRepaTuple xx;
+			xx = recordListsHistoryRepa_4(8, *qq);
+			_uu = std::move(std::get<0>(xx));
+			_ur = std::move(std::get<1>(xx));
+			ll.push_back(std::move(std::get<2>(xx)));
+		}
+		hr = vectorHistoryRepasConcat_u(ll);
+	}
+
+	auto& llu = _ur->listVarSizePair;
+	{
+		std::unique_ptr<Alignment::SystemRepa> ur1;
+		StrVarPtrMap m;
+		std::ifstream in(model + ".dr", std::ios::binary);
+		ur1 = persistentsSystemRepa(in, m);
+		_dr = persistentsApplicationRepa(in);
+		in.close();
+		auto& llu1 = ur1->listVarSizePair;			
+		SizeSizeUMap nn;
+		for (auto& ll : _dr->fud->layers)
+			for (auto& tr : ll)
+			{
+				auto x = tr->derived;
+				auto& p = llu1[x];
+				llu.push_back(VarSizePair(p.first, p.second));
+				nn[x] = llu.size() - 1;
+			}
+		_dr->reframe_u(nn);
+	}	
+
+	VarSet vvl;
+	vvl.insert(Variable("motor"));
+	vvl.insert(Variable("location"));
+	vvl.insert(Variable("room_next"));
+
+	auto& vvi = _ur->mapVarSize();
+	SizeList vvl1;
+	for (auto& v : vvl)
+		vvl1.push_back(vvi[v]);
+		
+	{
+		auto hr1 = frmul(*hr, *_dr->fud);
+		auto hr2 = hrhrred(vvl1.size(), vvl1.data(), *hr);
+		if (hr1->evient)
+			hr1->transpose();
+		auto z = hr1->size;
+		auto& mvv = hr1->mapVarInt();
+		auto sh = hr1->shape;
+		auto rr = hr1->arr;
+		auto nn = treesLeafNodes(*_dr->slices);
+		for (auto& s : *nn)
+		{
+			SizeList ev;
+			auto pk = mvv[s.first];
+			for (std::size_t j = 0; j < z; j++)
+			{
+				std::size_t u = rr[pk*z + j];
+				if (u)
+				{
+					ev.push_back(j);
+				}
+			}
+			if (ev.size() > 0)
+				_shr[s.first] = std::move(hrsel(ev.size(), ev.data(), *hr2));
+		}
+	}
+
+	_scan_sub = this->create_subscription<sensor_msgs::msg::LaserScan>(
+		"scan", rclcpp::SensorDataQoS(), std::bind(&Actor::scan_callback, this, std::placeholders::_1));
+	_odom_sub = this->create_subscription<nav_msgs::msg::Odometry>(
+		"odom", rclcpp::QoS(rclcpp::KeepLast(10)), std::bind(&Actor::odom_callback, this, std::placeholders::_1));
+
+	_observe_timer = this->create_wall_timer(observe_interval, std::bind(&Actor::observe_callback, this));
+
+	RCLCPP_INFO(this->get_logger(), "TBOT01 actor node has been initialised");
+}
+
+Actor::~Actor()
+{
+	RCLCPP_INFO(this->get_logger(), "TBOT01 observer node has been terminated");
+}
+
+void Actor::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+	_record.sensor_pose[0] = msg->pose.pose.position.x;
+	_record.sensor_pose[1] = msg->pose.pose.position.y;
+	_record.sensor_pose[2] = msg->pose.pose.position.z;
+	_record.sensor_pose[3] = msg->pose.pose.orientation.x;
+	_record.sensor_pose[4] = msg->pose.pose.orientation.y;
+	_record.sensor_pose[5] = msg->pose.pose.orientation.z;
+	_record.sensor_pose[6] = msg->pose.pose.orientation.w;
+	_pose_updated = true;
+}
+
+void Actor::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+	for (std::size_t i = 0; i < 360; i++)
+	{
+		if (std::isinf(msg->ranges.at(i)))
+			_record.sensor_scan[i] = msg->range_max;
+		else
+			_record.sensor_scan[i] = msg->ranges.at(i);
+	}
+	_scan_updated = true;
+}
+
+void Actor::observe_callback()
+{
+	typedef std::tuple<string, string, string> String3;			
+	typedef std::vector<String3> String3List;			
+
+	auto state = [](const Variable& v, const Value& u)
+	{
+		return State(VarValPairList{VarValPair(v, u)});
+	};
+	auto single = histogramSingleton_u;		
+	auto mul = pairHistogramsMultiply;
+	auto size = [](const Histogram& aa)
+	{
+		return (double)histogramsSize(aa).getNumerator();
+	};		
+	auto trim = histogramsTrim;
+	auto aall = histogramsList;
+	auto smax = [](const Histogram& aa)
+	{
+		std::vector<std::pair<Rational,State>> ll;
+		auto ll0 = *histogramsList(aa);
+		for (auto p : ll0)
+			ll.push_back(std::pair<Rational,State>(p.second,p.first));
+		auto ll1 = sorted(ll);
+		return ll1.back().second;
+	};		
+	auto ared = [](const Histogram& aa, const VarUSet& vv)
+	{
+		return setVarsHistogramsReduce(vv, aa);
+	};		
+	auto add = pairHistogramsAdd_u;
+	auto araa = systemsHistogramRepasHistogram_u;
+	auto hrsel = eventsHistoryRepasHistoryRepaSelection_u;
+	auto hrhrred = setVarsHistoryRepasHistoryRepaReduced_u;
+	auto hrred = setVarsHistoryRepasReduce_u;
+	auto frmul = historyRepasFudRepasMultiply_u;
+		
+	if (_pose_updated && _scan_updated)
+	{
+		String3List ll{
+			String3("room1","door12","room1"),
+			String3("room1","door13","room1"),
+			String3("room1","door14","room1"),
+			String3("room1","door45","room4"),
+			String3("room1","door56","room5"),
+			String3("room1","room1","room1"),
+			String3("room1","room2","room1"),
+			String3("room1","room3","room1"),
+			String3("room1","room4","room1"),
+			String3("room1","room5","room4"),
+			String3("room1","room6","room5"),
+			
+			String3("room2","door12","room2"),
+			String3("room2","door13","room1"),
+			String3("room2","door14","room1"),
+			String3("room2","door45","room4"),
+			String3("room2","door56","room5"),
+			String3("room2","room1","room2"),
+			String3("room2","room2","room2"),
+			String3("room2","room3","room1"),
+			String3("room2","room4","room1"),
+			String3("room2","room5","room4"),
+			String3("room2","room6","room5"),
+			
+			String3("room3","door12","room1"),
+			String3("room3","door13","room3"),
+			String3("room3","door14","room1"),
+			String3("room3","door45","room4"),
+			String3("room3","door56","room5"),
+			String3("room3","room1","room3"),
+			String3("room3","room2","room1"),
+			String3("room3","room3","room3"),
+			String3("room3","room4","room1"),
+			String3("room3","room5","room4"),
+			String3("room3","room6","room5"),
+			
+			String3("room4","door12","room1"),
+			String3("room4","door13","room2"),
+			String3("room4","door14","room4"),
+			String3("room4","door45","room4"),
+			String3("room4","door56","room5"),
+			String3("room4","room1","room4"),
+			String3("room4","room2","room1"),
+			String3("room4","room3","room1"),
+			String3("room4","room4","room4"),
+			String3("room4","room5","room4"),
+			String3("room4","room6","room5"),
+			
+			String3("room5","door12","room1"),
+			String3("room5","door13","room1"),
+			String3("room5","door14","room4"),
+			String3("room5","door45","room5"),
+			String3("room5","door56","room5"),
+			String3("room5","room1","room4"),
+			String3("room5","room2","room1"),
+			String3("room5","room3","room1"),
+			String3("room5","room4","room5"),
+			String3("room5","room5","room5"),
+			String3("room5","room6","room5"),
+			
+			String3("room6","door12","room1"),
+			String3("room6","door13","room1"),
+			String3("room6","door14","room4"),
+			String3("room6","door45","room5"),
+			String3("room6","door56","room6"),
+			String3("room6","room1","room4"),
+			String3("room6","room2","room1"),
+			String3("room6","room3","room1"),
+			String3("room6","room4","room5"),
+			String3("room6","room5","room6"),
+			String3("room6","room6","room6")
+		};
+		
+		Variable motor("motor");
+		Variable location("location");
+		Variable room_next("room_next");
+
+			
+		VarSet vvl;
+		vvl.insert(motor);
+		vvl.insert(location);
+		vvl.insert(room_next);
+
+		auto& vvi = _ur->mapVarSize();
+		SizeList vvl1;
+		for (auto& v : vvl)
+			vvl1.push_back(vvi[v]);
+		
+		std::size_t s = 0;
+		{
+			auto xx = recordListsHistoryRepa_4(8, RecordList{ _record });
+			auto hr = std::move(std::get<2>(xx));
+			SizeList ww;
+			auto nn = treesLeafNodes(*_dr->slices);
+			for (auto& s : *nn)
+				ww.push_back(s.first);
+			auto hr1 = hrhrred(ww.size(), ww.data(), *frmul(*hr, *_dr->fud));
+			auto n = hr1->dimension;
+			auto vv = hr1->vectorVar;
+			auto rr = hr1->arr;
+			bool found = false;
+			for (std::size_t i = 0; !found && i < n; i++)
+			{
+				std::size_t u = rr[i];
+				if (u)
+				{
+					s = vv[i];
+					found = true;
+				}
+			}	
+			if (!found)
+			{
+				RCLCPP_INFO(this->get_logger(), "observe_callback: error: no slice");
+				return;
+			}
+		}
+		if (_shr.find(s) != _shr.end())
+		{
+			auto aa = *trim(*araa(*_uu, *_ur, *_shr[s]));
+
+		}
+		
+		std::vector<std::string> locations{ "door12", "door13", "door14", "door45", "door56",
+			"room1", "room2", "room3", "room4", "room5", "room6", "unknown" };
+		std::vector<std::string> positions{ "centre", "corner", "side", "unknown" };
+		bool is_match = l == cl;
+		if (is_match)
+			_matches++;
+		_observations++;
+
+		std::string report;
+		report += _label == "location" ? locations[cl] : positions[cl];
+		report += "\t";
+		report += _label == "location" ? locations[l] : positions[l];
+		report += "\t";
+		report += l == cl ? "match" : "fail";
+		report += "\t";
+		report += std::to_string((double)_matches / (double)_observations * 100.0);
+
+		RCLCPP_INFO(this->get_logger(), report);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	std::string model = string(argc >= 2 ? argv[1] : "model006_location");
+	std::string room_initial = string(argc >= 3 ? argv[2] : "room1");
+	std::chrono::milliseconds observe_interval(argc >= 4 ? std::atol(argv[3]) : 5*60);
+	string dataset = string(argc >= 5 ? argv[4] : "data002");
+
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<Actor>(model, room_initial, observe_interval, dataset));
+	rclcpp::shutdown();
+
+	return 0;
+}
+
---
+++ actor.h
@@ -0,0 +1,38 @@
+﻿#ifndef OBSERVER_H
+#define OBSERVER_H
+
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+
+#include "dev.h"
+
+class Actor : public rclcpp::Node
+{
+public:
+	Actor(const std::string&, const std::string&, std::chrono::milliseconds, const std::string&);
+	~Actor();
+
+private:
+	rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr _scan_sub;
+	rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr _odom_sub;
+
+	rclcpp::TimerBase::SharedPtr _observe_timer;
+
+	TBOT01::Record _record;
+	bool _pose_updated;
+	bool _scan_updated;
+	
+	string _room;
+
+	std::unique_ptr<Alignment::System> _uu;
+	std::unique_ptr<Alignment::SystemRepa> _ur;
+	std::unique_ptr<Alignment::ApplicationRepa> _dr;
+	std::map<std::size_t, std::shared_ptr<Alignment::History>> _shr;
+
+	void observe_callback();
+	void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+	void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+};
+#endif // OBSERVER_H
